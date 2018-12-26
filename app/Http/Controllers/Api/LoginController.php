8fbd23e239761b16125a4aa24573bb10324e46ff
LeoYang <aileshang0226@163.com>
2018-12-26 15:18:49 +0800

    init

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Api\LoginRequest;
 use App\Http\Service\AuthService;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class LoginController extends Controller
